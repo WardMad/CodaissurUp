f1c0a69a9a5ae618fa3da943c2319649d8698378
ward <vakpko@gmail.com>
2017-11-10 16:14:40 +0100

    One-to-One Association

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   resources :categories
+  resources :profiles, only: [:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
